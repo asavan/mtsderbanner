c9472229db95e0fa778124f6fac81b422ef3c45a
a.savanovich <asavanster@gmail.com>
2017-06-05 02:09:51 +0300

    good icon

--- src/main/java/mtstest/SmartfonInfo.java
+++ src/main/java/mtstest/SmartfonInfo.java
@@ -3,47 +3,45 @@ package mtstest;
 import java.util.Map;
 
 
-public class SmartfonInfo
-{
-  private String articul;
-  private String mainPhoto;
-  private String originalUrl;
-  Map<String, String> properties;
-  
-  public SmartfonInfo() {}
-  
-  public String getOriginalUrl()
-  {
-    return originalUrl;
-  }
-  
-  public void setOriginalUrl(String originalUrl) {
-    this.originalUrl = originalUrl;
-  }
-  
-  public Map<String, String> getProperties() {
-    return properties;
-  }
-  
-  public void setProperties(Map<String, String> properties) {
-    this.properties = properties;
-  }
-  
+public class SmartfonInfo {
+    private String articul;
+    private String mainPhoto;
+    private String originalUrl;
+    private Map<String, String> properties;
 
-  public String getArticul()
-  {
-    return articul;
-  }
-  
-  public void setArticul(String articul) {
-    this.articul = articul;
-  }
-  
-  public String getMainPhoto() {
-    return mainPhoto;
-  }
-  
-  public void setMainPhoto(String mainPhoto) {
-    this.mainPhoto = mainPhoto;
-  }
+    public SmartfonInfo() {
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    public void setProperties(Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+
+    public String getArticul() {
+        return articul;
+    }
+
+    public void setArticul(String articul) {
+        this.articul = articul;
+    }
+
+    public String getMainPhoto() {
+        return mainPhoto;
+    }
+
+    public void setMainPhoto(String mainPhoto) {
+        this.mainPhoto = mainPhoto;
+    }
 }
